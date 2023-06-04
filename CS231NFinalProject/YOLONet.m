16ff9063b0ecff75acb5217c90965060a7b5438b
Luc Rosenzweig <lucrosenzweig@DN0a1f4398.SUNet>
2023-06-03 21:45:23 -0700

    a

--- CS231NFinalProject/YOLONet.m
+++ CS231NFinalProject/YOLONet.m
@@ -28,6 +28,7 @@
     MPSNNImageNode *inputImageNode = [[MPSNNImageNode alloc] initWithHandle:nil];
     MPSNNLanczosScaleNode *scaleNode = [[MPSNNLanczosScaleNode alloc] initWithSource:inputImageNode outputSize:MTLSizeMake(mInputWidth, mInputHeight, 1)];
     
+#if 0
     // Convolutions.
     MPSCNNConvolutionNode *convNode1 = [[MPSCNNConvolutionNode alloc] initWithSource:scaleNode.resultImage weights:[[NetParams alloc] init:@"conv1" kernelSize:3 inputFeatureChannels:3 outputFeatureChannels:16]];
     MPSCNNPoolingMaxNode *poolNode1 = [[MPSCNNPoolingMaxNode alloc] initWithSource:convNode1.resultImage filterSize:2];
@@ -53,10 +54,13 @@
     MPSCNNConvolutionNode *convNode8 = [[MPSCNNConvolutionNode alloc] initWithSource:convNode7.resultImage weights:[[NetParams alloc] init:@"conv8" kernelSize:3 inputFeatureChannels:1024 outputFeatureChannels:1024]];
     
     MPSCNNConvolutionNode *convNode9 = [[MPSCNNConvolutionNode alloc] initWithSource:convNode8.resultImage weights:[[NetParams alloc] init:@"conv9" kernelSize:1 inputFeatureChannels:1024 outputFeatureChannels:125]];
+#endif
     
-    NSArray *result = @[convNode9.resultImage];
+    NSArray *result = @[scaleNode.resultImage];
     BOOL needed = true;
     mNetGraph = [[MPSNNGraph alloc] initWithDevice:device resultImages:result resultsAreNeeded:&needed];
+    
+    NSLog(@"%@", mNetGraph.debugDescription);
 }
 
 -(void)encodeGraph:(nonnull id<MTLTexture>)inputTexture commandBuffer:(id<MTLCommandBuffer>)cmdbuf
